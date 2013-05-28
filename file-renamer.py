f5a9f1de462131fb818c4e11d56b3cb649dc722b
Ali BARIN <ali.barin53@gmail.com>
2013-05-28 21:41:54 +0300

    first commit

---
+++ file-renamer.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#
+# Ali BARIN
+#
+# Examples:  
+##  
+##  Sample file list: \example\a.txt, \example\b.txt, \example\c.txt,
+##                      \example\d.txt, \example\e.jpg
+##
+##  root path: \example
+##  file prefix: example-prefix-
+##
+##  after proccessing:  \example\example-prefix-1.txt, \example\example-prefix-2.txt,
+##                      \example\example-prefix-3.txt, \example\example-prefix-4.txt,
+##                      \example\example-prefix-5.jpg
+##
+
+import os
+
+root_path = input("Root path: ")
+file_prefix = input("File prefix: ")
+
+paths = []
+for (root, paths, files) in os.walk(root_path):
+    for index, file in enumerate(files):
+        index += 1
+        full_path_file = root + "\\" + file
+        file = file.split(".")
+        file = { "name": file[0], "ext": file[1] }
+        full_path_new_file = root + "\\" + file_prefix + str(index) + "." + file["ext"]
+        os.rename(full_path_file, full_path_new_file)
+
+print("All file names changed with its new names.")
