72d910313136488190036dc15695a66601751881
Ali BARIN <ali.barin53@gmail.com>
2013-06-07 23:51:56 +0300

    fix path name and excluded not named files.

--- file-renamer.py
+++ file-renamer.py
@@ -22,12 +22,13 @@ file_prefix = input("File prefix: ")
 
 paths = []
 for (root, paths, files) in os.walk(root_path):
-    for index, file in enumerate(files):
-        index += 1
-        full_path_file = root + "\\" + file
-        file = file.split(".")
-        file = { "name": file[0], "ext": file[1] }
-        full_path_new_file = root + "\\" + file_prefix + str(index) + "." + file["ext"]
-        os.rename(full_path_file, full_path_new_file)
+		for index, file in enumerate(files):
+				index += 1
+				full_path_file = root + "/" + file
+				file = file.split(".")
+				if not len(file) == 1:
+					file = { "name": file[0], "ext": file[1] }
+					full_path_new_file = root + "/" + file_prefix + str(index) + "." + file["ext"]
+					os.rename(full_path_file, full_path_new_file)
 
 print("All file names changed with its new names.")
